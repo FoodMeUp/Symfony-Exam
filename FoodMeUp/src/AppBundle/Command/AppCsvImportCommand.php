ba47c76cf338466d7ef1915f20bde5985341f917
nissim94 <nissim94@hotmail.fr>
2017-11-14 14:44:03 +0100

    Adds the process to persist the data into the database

--- FoodMeUp/src/AppBundle/Command/AppCsvImportCommand.php
+++ FoodMeUp/src/AppBundle/Command/AppCsvImportCommand.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Entity\Ingredient;
+use AppBundle\Entity\IngredientFamily;
 use Doctrine\ORM\EntityManagerInterface;
 use League\Csv\Reader;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -40,9 +42,46 @@ class AppCsvImportCommand extends ContainerAwareCommand
         $CSVReader->setDelimiter(";");
         $CSVReader->setHeaderOffset(0);
 
-        foreach ($CSVReader as $row)
-        {
+        $currentIngredientFamilyId = null;
+        $IngredientFamily = null;
 
+        foreach ($CSVReader as $line)
+        {
+            // if the ingredient's family happen for the first time we persist it.
+            if ($line['ORIGGPCD'] !== $currentIngredientFamilyId)
+            {
+                if (isset($IngredientFamily))
+                {
+                    $this->em->persist($IngredientFamily);
+                    $this->em->flush();
+                }
+
+                $IngredientFamily = new IngredientFamily($line['ORIGGPFR']);
+                $currentIngredientFamilyId = $line['ORIGGPCD'];
+            }
+
+            $ingredientName = $line['ORIGFDNM'];
+            $energies = [];
+            $nutrients = [];
+
+            foreach ($line as $key => $value) {
+                // clean the blank spaces
+                $value = trim($value);
+
+                // if it's an energy, we are verifying if it prefixed by Energie, ...
+                if (preg_match('/^Energie, (.+?)$/', $key, $matches))
+                {
+                    $energies[$matches[1]] = $value;
+                }
+
+                // others are nutrients
+                $nutrients[$key] = $value;
+            }
+
+            // now persist our ingredients
+            $ingredient = new Ingredient($ingredientName, $energies, $nutrients, $IngredientFamily);
+            // adding ingredient to the collection
+            $IngredientFamily->addIngredient($ingredient);
         }
 
         $io->success("The CVS has been successfully saved into the database");
